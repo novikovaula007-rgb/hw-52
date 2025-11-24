65b81c31d7664d85ffae1bf2df441e21e330d010
Novikova Ulyana <novikova.ula.007@gmail.com>
2025-11-24 20:00:56 +0300

    [hw-52] pokerhand

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,43 @@ import Card from './components/Card/Card.tsx'
 import type {ICard} from './types'
 import {useState} from "react";
 import CardDeck from "./lib/CardDeck.ts";
+import PokerHand from "./lib/PokerHand.ts";
 
 
 function App() {
     const [myHand, setMyHand] = useState<ICard[]>([])
     const [Deck, setDeck] = useState(new CardDeck())
-    let buttonState: string
-
-    if (Deck.cardDeck.length > 0) {
-        buttonState = 'Взять 5 карт'
-    } else {
-        buttonState = 'Раздать карты'
-    }
+    const [combination, setCombination] = useState<string>('')
 
     const loadCards = () => {
+        if (Deck.cardDeck.length <= 0) {
+            setDeck(new CardDeck())
+            setMyHand([])
+            setCombination('')
+        }
+    }
+
+    const takeFiveCards = () => {
         if (Deck.cardDeck.length > 0) {
             const cardsToHand = Deck.getCards(5)
             setMyHand(cardsToHand)
-        } else {
-            setDeck(new CardDeck())
-            setMyHand([])
+            if (cardsToHand.length === 5) {
+                const pokerHand = new PokerHand(cardsToHand)
+                const pokerCombination = pokerHand.getOutcome()
+                setCombination(pokerCombination)
+            }
         }
     }
+
+
 
     return (
         <>
             <p>Cards count: {Deck.cardDeck.length}</p>
             {Deck.cardDeck.length > 0?
                 <>
-                    <button onClick={loadCards}>{buttonState}</button>
+                    <p>Combination of round: {combination}</p>
+                    <button onClick={takeFiveCards}>Взять 5 карт</button>
                     <div className="playingCards faceImages">
                         {myHand.map((card: ICard, index) => (
                             <Card key={index} rank={card.rank} suit={card.suit}/>
@@ -41,7 +49,7 @@ function App() {
                 :
                 <>
                     <p>Колода пустая</p>
-                    <button onClick={loadCards}>{buttonState}</button>
+                    <button onClick={loadCards}>Раздать карты</button>
                 </>
             }
         </>
--- src/lib/PokerHand.ts
+++ src/lib/PokerHand.ts
@@ -1,14 +1,106 @@
-import type Card from './Card.ts'
+import type {ICard} from '../types'
+
+const rankToNumbers = {
+    '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
+    '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14, 'T': 10
+}
 
 class PokerHand {
-    public myHand: Card[]
-    constructor(myHand: Card[]) {
+    public myHand: ICard[]
+    public numberRanks: number[]
+    public countRanks: {[key: number]: number} = {}
+    public countSuits: {[key: string]: number} = {}
+    constructor(myHand: ICard[]) {
         this.myHand = myHand
+        console.log(myHand)
+
+        this.numberRanks = this.myHand.map(card => {
+            return rankToNumbers[card.rank]
+        })
+
+        this.numberRanks.forEach(rank => {
+            if (this.countRanks[rank]) {
+                this.countRanks[rank] ++
+            } else {
+                this.countRanks[rank] = 1
+            }
+        })
+
+        this.myHand.forEach(card => {
+            if (this.countSuits[card.suit]) {
+                this.countSuits[card.suit] ++
+            } else {
+                this.countSuits[card.suit] = 1
+            }
+        })
     }
 
-    hetOutcome() {
-        //поиск карты
-        return ''
+    getOutcome() {
+        if (this.numberRanks.length !== 5) {
+            throw new Error('A combination can only consist of 5 cards.')
+        }
+
+        const cardsDescending = this.numberRanks.sort((a, b) => b - a)
+        const countRanksDescending = Object.values(this.countRanks).sort((a, b) => b - a)
+
+        let isCardsOneSuit = true
+        if (!Object.values(this.countSuits).includes(5)) {
+            isCardsOneSuit = false
+        }
+
+        let isCardsInOrder = true
+
+        for (let i = 0; i < 4; i++) {
+            if (cardsDescending[i] !== cardsDescending[i+1] + 1) {
+                isCardsInOrder = false
+                break
+            }
+        }
+
+        if (isCardsInOrder && isCardsOneSuit) {
+            if (cardsDescending[0] == 14) {
+                return 'Royal flush'
+            } else {
+                return 'Straight flush'
+            }
+        }
+
+        if (countRanksDescending[0] == 4) {
+            return 'Four of a kind'
+        }
+
+        if (countRanksDescending[0] == 3 && countRanksDescending[1] == 2) {
+            return 'Full house'
+        }
+
+        if (isCardsOneSuit) {
+            return 'Flush'
+        }
+
+        let IsStraight = false
+        if (cardsDescending[0] == 14) {
+            if ([5, 4, 3, 2].every(rank => cardsDescending.includes(rank))) {
+                IsStraight = true
+            }
+        }
+
+        if (IsStraight || isCardsInOrder) {
+            return 'Straight'
+        }
+
+        if (countRanksDescending[0] == 3) {
+            return 'Three of a kind'
+        }
+
+        if (countRanksDescending[0] == 2 && countRanksDescending[1] == 2) {
+            return 'Two pairs'
+        }
+
+        if (countRanksDescending[0] == 2) {
+            return 'One pair'
+        }
+
+        return 'High card'
     }
 }
 
